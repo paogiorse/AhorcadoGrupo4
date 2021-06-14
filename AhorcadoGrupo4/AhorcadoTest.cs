9f65096f7f15f61e36d38478bc7ea09f2ad3ff01
Giorsetti, Paola <paola.giorsetti@accenture.com>
2021-06-14 17:37:25 -0300

    first commit

---
+++ AhorcadoGrupo4/AhorcadoTest.cs
@@ -0,0 +1,159 @@
+using AhorcadoGrupo4Codigo;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AhorcadoGrupo4
+{
+    public class AhorcadoTest
+    {
+        //Ingreso Jugador
+
+        //Jugador anonimo
+        [Fact]
+        public void JugadorAnonimo()
+        {
+            //Arrange
+            var juego = new Jugada();
+            juego.nombreJugador = "Anónimo";
+            //Act
+            var jugador = juego.IngresoJugador();
+            //Assert
+            Assert.Equal(juego.nombreJugador, jugador);
+        }
+
+
+        //Seleccionar dificultad
+
+        //Jugada sin dificultad 
+        [Fact]
+        public void JugadaSinDificultad()
+        {
+            //Arrange
+            var juego = new Jugada();
+            juego.dificultad = "SinDificultad";
+            //Act
+            var dificultad = juego.SeleccionarDificultad();
+            //Assert
+            Assert.Equal(juego.dificultad, dificultad);
+        }
+
+
+        //Generar palabra hardcodeada
+
+        //Probar palabra generada
+        [Fact]
+        public void ProbarPalabraGenerada()
+        {
+            //Arrange
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gato";
+            //Act
+            string palabraGenerada = juego.GenerarPalabra();
+            //Assert
+            Assert.Equal(juego.palabraHardcodeada, palabraGenerada);
+        }
+
+        //Ingresar letra y verificar si pertenece a la palabra
+
+        //String vacio
+        [Fact]
+        public void ProbarIngresarLetraStringVacio()
+        {
+            //Arrange
+            string letra = string.Empty;
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gato";
+            //Act
+            bool pertenece = juego.PerteneceLetraPalabra(letra);
+            //Assert
+            Assert.False(pertenece);
+        }
+        //Letra pertenece
+        [Fact]
+        public void ProbarIngresarLetraPertenece()
+        {
+            //Arrange
+            string letra = "g";
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gato";
+            //Act
+            bool pertenece = juego.PerteneceLetraPalabra(letra);
+            //Assert
+            Assert.True(pertenece);
+        }
+        //Letra no pertenece
+        [Fact]
+        public void ProbarIngresarLetraNoPertenece()
+        {
+            //Arrange
+            string letra = "h";
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gato";
+            //Act
+            bool pertenece = juego.PerteneceLetraPalabra(letra);
+            //Assert
+            Assert.False(pertenece);
+        }
+
+        //Mostrar palabra
+
+        //Comparar la palabra generada con la parcial
+        [Fact]
+        public void CompararPalabra()
+        {
+            //Arrange
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gatoga";
+            juego.palabraParcial = new char[juego.palabraHardcodeada.Length];
+            juego.palabraParcial[0] = 'g';
+            juego.palabraParcial[1] = 'a';
+            juego.palabraParcial[4] = 'g';
+            juego.palabraParcial[5] = 'a';
+            List<char> letrasIngresadas = new List<char>() { 'e', 'p', 'g', 'a' };
+            //Act
+            var palabraParcial = juego.MostrarPalabra(juego.palabraHardcodeada, letrasIngresadas);
+            //Assert
+            Assert.Equal(juego.palabraParcial, palabraParcial);
+        }
+
+        //Terminar la partida
+
+        //Partida terminada
+        [Fact]
+        public void PartidaTerminada()
+        {
+            //Arrange
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gato";
+            juego.palabraParcial = new char[juego.palabraHardcodeada.Length];
+            juego.palabraParcial[0] = 'g';
+            juego.palabraParcial[1] = 'a';
+            juego.palabraParcial[2] = 't';
+            juego.palabraParcial[3] = 'o';
+            juego.estado = "En curso";
+            var estadoFinal = "Terminado";
+            //Act
+            var estadoPartida = juego.TerminarPartida(juego.palabraParcial, juego.palabraHardcodeada);
+            //Assert
+            Assert.Equal(estadoFinal, estadoPartida);
+        }
+
+        //Partida no terminada
+        [Fact]
+        public void PartidaNoTerminada()
+        {
+            //Arrange
+            var juego = new Jugada();
+            juego.palabraHardcodeada = "gato";
+            juego.palabraParcial = new char[juego.palabraHardcodeada.Length];
+            juego.palabraParcial[0] = 'g';
+            juego.palabraParcial[1] = 'a';
+            juego.estado = "En curso";
+            //Act
+            var estadoPartida = juego.TerminarPartida(juego.palabraParcial, juego.palabraHardcodeada);
+            //Assert
+            Assert.Equal(juego.estado, estadoPartida);
+        }
+    }
+}
---
+++ AhorcadoGrupo4Codigo/Jugada.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AhorcadoGrupo4Codigo
+{
+    public class Jugada
+    {
+        public string palabraHardcodeada;
+        public string estado;
+        public string nombreJugador;
+        public string dificultad;
+        public char[] palabraParcial;
+        public string GenerarPalabra()
+        {
+            return palabraHardcodeada;
+        }
+
+        public bool PerteneceLetraPalabra(string letra)
+        {
+            if(String.IsNullOrEmpty(letra))
+            {
+                return false;
+            }
+            else if(palabraHardcodeada.Contains(letra))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public char[] MostrarPalabra(string palabra, List<char> letrasIngresadas)
+        {
+            char[] palabraParcial = new char[palabra.Length];
+            foreach (var letra in letrasIngresadas)
+            {
+                int minIndex = 0;
+                if (palabra.Contains(letra))
+                {
+                    do
+                    {
+                        minIndex = palabra.IndexOf(letra, minIndex);
+                        if (minIndex != -1)
+                        {
+                            palabraParcial[minIndex] = letra;
+                            minIndex++;
+                        }
+                    } while (minIndex != -1);
+                }
+            }
+            return palabraParcial;
+        }
+
+        public string TerminarPartida(char[] parcial, string harcodeada)
+        {
+            if (new String(parcial) == harcodeada)
+                estado = "Terminado";
+            return estado;
+        }
+
+        public string IngresoJugador()
+        {
+            return nombreJugador;
+        }
+
+        public string SeleccionarDificultad()
+        {
+            return dificultad;
+        }
+    }
+}
